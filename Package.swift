da30265155fbe7c3e933a131f82b39922e3c30e8
SOFIENE BOUHLEL <sbouhlel.olympp@tf1.fr>
2021-12-29 18:41:58 +0100

    update Freewheel Package

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Freewheel",
-            targets: ["Freewheel", "FreewheelLib"]),
+            targets: ["Freewheel"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,13 +19,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Freewheel",
-            dependencies: []),
         .binaryTarget(
-            name: "FreewheelLib",
-            path: "Sources/Freewheel/AdManager.xcframework"
+            name: "Freewheel",
+            path: "AdManager.xcframework"
         )
-        
     ]
 )
--- Sources/Freewheel/Freewheel.swift
+++
@@ -1,6 +0,0 @@
-public struct Freewheel {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
