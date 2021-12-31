da52f4e3ad662c4c1ab7a210312ffcf21814c44c
SOFIENE BOUHLEL <sbouhlel.olympp@tf1.fr>
2021-12-31 11:21:14 +0100

    update Freewheel to a dynamic framework

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Freewheel",
+            type: .dynamic,
             targets: ["Freewheel"]),
     ],
     dependencies: [
